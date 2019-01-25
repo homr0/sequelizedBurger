76a9857a277c1960823f716170858cf7db1c0896
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-25 15:30:25 -0800

    configured server

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependecies
+var express = require("express");
+
+// Server configuration
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serves static files from the public directory.
+app.use(express.static("public"));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// Sets up Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
