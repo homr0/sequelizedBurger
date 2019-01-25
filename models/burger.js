965681c20037570cb333a9b7cbde8e2bacbdb3e4
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-24 20:07:07 -0800

    added burger model

---
+++ models/burger.js
@@ -0,0 +1,25 @@
+// Dependencies
+var orm = require("../config/orm.js");
+
+// Calls ORM functions specific to burger
+var burger = {
+    selectAll: (cb) => {
+        orm.selectAll("burgers", (res) => {
+            cb(res);
+        });
+    },
+
+    insertOne: (cols, vals, cb) => {
+        orm.insertOne("burgers", cols, vals, (res) => {
+            cb(res);
+        })
+    },
+
+    updateOne: (objColVals, condition, cb) => {
+        orm.updateOne("burgers", objColVals, condition, (res) => {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
