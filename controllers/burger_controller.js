21a59657feb65498407aa312390dd9c83ba0ecd1
Rebecca Hom <rebecca.hom001@gmail.com>
2019-01-24 20:50:51 -0800

    added controller for routes for viewing, creating, and updating burgers

---
+++ controllers/burger_controller.js
@@ -0,0 +1,39 @@
+// Dependencies
+var express = require("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+// Routes
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        var hbsObject = {
+            burgers: data
+        };
+
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne([
+        "burger_name", "devoured"
+    ], [req.body.burger_name, req.body.devoured], (data) => {
+        // Send back the id of the newly created burger.
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, (data) => {
+        if(result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist.
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
